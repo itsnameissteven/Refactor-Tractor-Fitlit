558bb6497ebbd872d2cca1c78c7dc07f4df5f70a
Steven Mancine <stevenrmancine@gmail.com>
2021-02-23 08:51:56 -0700

    Refactor user test

--- src/User-repo.js
+++ src/User-repo.js
@@ -12,15 +12,15 @@ class UserRepo {
   };
 
   calculateAverageStepGoal() {
-    var totalStepGoal = this.users.reduce((sumSoFar, data) => {
+    const totalStepGoal = this.users.reduce((sumSoFar, data) => {
       return sumSoFar = sumSoFar + data.dailyStepGoal;
     }, 0);
     return totalStepGoal / this.users.length;
   };
 
   makeSortedUserArray(id, dataSet) {
-    let selectedID = this.getDataFromUserID(id, dataSet)
-    let sortedByDate = selectedID.sort((a, b) => new Date(b.date) - new Date(a.date));
+    const selectedID = this.getDataFromUserID(id, dataSet);
+    const sortedByDate = selectedID.sort((a, b) => new Date(b.date) - new Date(a.date));
     return sortedByDate;
   }
 
@@ -33,20 +33,21 @@ class UserRepo {
   };
 
   getWeekFromDate(date, id, dataSet) {
-    let searchedDate = this.makeSortedUserArray(id, dataSet).find((sortedItem) => (sortedItem.date === date))
-    let dateIndex = this.makeSortedUserArray(id, dataSet).indexOf(searchedDate);
+    const searchedDate = this.makeSortedUserArray(id, dataSet).find((sortedItem) => (sortedItem.date === date));
+    const dateIndex = this.makeSortedUserArray(id, dataSet).indexOf(searchedDate);
     return this.makeSortedUserArray(id, dataSet).slice(dateIndex, dateIndex + 7);
   };
 
   chooseWeekDataForAllUsers(dataSet, date) {
-    return dataSet.filter(function(dataItem) {
-      return (new Date(date)).setDate((new Date(date)).getDate() - 7) <= new Date(dataItem.date) && new Date(dataItem.date) <= new Date(date)
+    const lastWeek = new Date(date).setDate((new Date(date)).getDate() - 7)
+    return dataSet.filter((dataItem) => {
+      return lastWeek <= new Date(dataItem.date) && new Date(dataItem.date) <= new Date(date);
     })
   };
 
   chooseDayDataForAllUsers(dataSet, date) {
     return dataSet.filter(function(dataItem) {
-      return dataItem.date === date
+      return dataItem.date === date;
     });
   }
 
--- test/user-test.js
+++ test/user-test.js
@@ -4,78 +4,6 @@ import UserRepo from '../src/User-repo';
 import User from '../src/User';
 
 describe('User', function() {
-
-  it('should be a function', function() {
-    const user = new User({
-      id: 1,
-      name: "Alex Roth",
-      address: "1234 Turing Street, Denver CO 80301-1697",
-      email: "alex.roth1@hotmail.com",
-      strideLength: 4.3,
-      dailyStepGoal: 10000,
-      friends: [2, 3, 4]
-    });
-    expect(User).to.be.a('function');
-  });
-
-  it('should be an instance of User', function() {
-    const user = new User({
-      id: 1,
-      name: "Alex Roth",
-      address: "1234 Turing Street, Denver CO 80301-1697",
-      email: "alex.roth1@hotmail.com",
-      strideLength: 4.3,
-      dailyStepGoal: 10000,
-      friends: [2, 3, 4]
-    });
-    expect(user).to.be.an.instanceof(User);
-  });
-
-  it('should take a user data object', function() {
-    const user = new User({
-      id: 1,
-      name: "Alex Roth",
-      address: "1234 Turing Street, Denver CO 80301-1697",
-      email: "alex.roth1@hotmail.com",
-      strideLength: 4.3,
-      dailyStepGoal: 10000,
-      friends: [2, 3, 4]
-    });
-
-    expect(user.id).to.equal(1);
-    expect(user.name).to.equal("Alex Roth");
-  });
-
-  it('should take a different user data object', function() {
-    const user2 = new User({
-      id: 2,
-      name: "Allie McCarthy",
-      address: "1235 Turing Street, Denver CO 80301-1697",
-      email: "allie.mcc1@hotmail.com",
-      strideLength: 3.3,
-      dailyStepGoal: 9000,
-      friends: [1, 3, 4]
-    });
-
-    expect(user2.id).to.equal(2);
-    expect(user2.name).to.equal("Allie McCarthy");
-  });
-
-  it('should return user first name', function() {
-    const user2 = new User({
-      id: 2,
-      name: "Allie McCarthy",
-      address: "1235 Turing Street, Denver CO 80301-1697",
-      email: "allie.mcc1@hotmail.com",
-      strideLength: 3.3,
-      dailyStepGoal: 9000,
-      friends: [1, 3, 4]
-    });
-
-    expect(user2.getFirstName()).to.equal("Allie");
-  });
-
-  it('should return list of friend names from user repository', function() {
     const user1 = new User({
       id: 1,
       name: "Alex Roth",
@@ -94,7 +22,6 @@ describe('User', function() {
       dailyStepGoal: 9000,
       friends: [1, 3, 4]
     });
-
     const user3 = new User({
       id: 3,
       name: "The Rock",
@@ -104,7 +31,6 @@ describe('User', function() {
       dailyStepGoal: 60000,
       friends: [1, 2, 4]
     });
-
     const user4 = new User({
       id: 4,
       name: "Rainbow Dash",
@@ -114,10 +40,35 @@ describe('User', function() {
       dailyStepGoal: 7000,
       friends: [1, 2, 3]
     });
+
+
+  it('should be a function', function() {
+    expect(User).to.be.a('function');
+    expect(user1).to.be.an.instanceof(User);
+  });
+
+  it('should take a user data object', function() {
+    expect(user1.id).to.deep.equal(1);
+    expect(user1.name).to.deep.equal("Alex Roth");
+    expect(user1.address).to.deep.equal("1234 Turing Street, Denver CO 80301-1697");
+    expect(user1.strideLength).to.deep.equal(4.3);
+    expect(user1.dailyStepGoal).to.deep.equal(10000);
+    expect(user1.friends).to.deep.equal([2, 3, 4]); 
+    expect(user1.email).to.deep.equal("alex.roth1@hotmail.com");
+  });
+
+  it('should take a different user data object', function() {
+    expect(user2.id).to.equal(2);
+    expect(user2.name).to.equal("Allie McCarthy");
+  });
+
+  it('should return users first name', function() {
+    expect(user2.getFirstName()).to.equal("Allie");
+  });
+
+  it('should return list of friend names from user repository', function() {
     const users = [user1, user2, user3, user4];
     const userRepo = new UserRepo(users);
-
-    console.log(user2.getFriendsNames(userRepo));
     expect(user2.getFriendsNames(userRepo)).to.deep.equal(['Alex Roth', 'The Rock', 'Rainbow Dash']);
   });
 });
