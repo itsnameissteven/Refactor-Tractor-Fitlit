9b309e932fac5eadc95a9a9042a29d6a91cc314c
Anneke Miers <annekemiers.am@gmail.com>
2021-02-23 17:28:16 -0700

    Change all tests to have arrow functions

--- test/hydration-test.js
+++ test/hydration-test.js
@@ -3,122 +3,127 @@ import Hydration from '../src/Hydration';
 import UserRepo from '../src/User-repo';
 import User from '../src/User';
 
-describe('Hydration', function() {
+describe('Hydration', () => {
   let hydrationData;
   let hydration;
 
-  beforeEach(function() {
+  beforeEach(() => {
     hydrationData = [{
-        "userID": 1,
-        "date": "2019/06/15",
-        "numOunces": 37
-      },
-      {
-        "userID": 2,
-        "date": "2019/06/15",
-        "numOunces": 38
-      },
-      {
-        "userID": 3,
-        "date": "2019/05/09",
-        "numOunces": 1
-      },
-      {
-        "userID": 4,
-        "date": "2019/04/15",
-        "numOunces": 36
-      },
-      {
-        "userID": 2,
-        "date": "2018/10/23",
-        "numOunces": 34
-      },
-      {
-        "userID": 1,
-        "date": "2018/06/16",
-        "numOunces": 39
-      },
-      {
-        "userID": 3,
-        "date": "2018/03/30",
-        "numOunces": 2
-      },
-      {
-        "userID": 4,
-        "date": "2018/02/01",
-        "numOunces": 28
-      },
-      {
-        "userID": 1,
-        "date": "2016/08/22",
-        "numOunces": 30
-      },
-      {
-        "userID": 3,
-        "date": "2016/05/14",
-        "numOunces": 3
-      },
-      {
-        "userID": 2,
-        "date": "2016/04/27",
-        "numOunces": 40
-      },
-      {
-        "userID": 4,
-        "date": "2019/03/15",
-        "numOunces": 35
-      },
-      {
-        "userID": 4,
-        "date": "2019/09/20",
-        "numOunces": 40
-      },
-      {
-        "userID": 4,
-        "date": "2019/09/19",
-        "numOunces": 30
-      },
-      {
-        "userID": 4,
-        "date": "2019/09/18",
-        "numOunces": 40
-      },
-      {
-        "userID": 4,
-        "date": "2019/09/17",
-        "numOunces": 40
-      },
-      {
-        "userID": 4,
-        "date": "2019/09/16",
-        "numOunces": 30
-      },
-      {
-        "userID": 4,
-        "date": "2019/09/15",
-        "numOunces": 30
-      },
+      "userID": 1,
+      "date": "2019/06/15",
+      "numOunces": 37
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/15",
+      "numOunces": 38
+    },
+    {
+      "userID": 3,
+      "date": "2019/05/09",
+      "numOunces": 1
+    },
+    {
+      "userID": 4,
+      "date": "2019/04/15",
+      "numOunces": 36
+    },
+    {
+      "userID": 2,
+      "date": "2018/10/23",
+      "numOunces": 34
+    },
+    {
+      "userID": 1,
+      "date": "2018/06/16",
+      "numOunces": 39
+    },
+    {
+      "userID": 3,
+      "date": "2018/03/30",
+      "numOunces": 2
+    },
+    {
+      "userID": 4,
+      "date": "2018/02/01",
+      "numOunces": 28
+    },
+    {
+      "userID": 1,
+      "date": "2016/08/22",
+      "numOunces": 30
+    },
+    {
+      "userID": 3,
+      "date": "2016/05/14",
+      "numOunces": 3
+    },
+    {
+      "userID": 2,
+      "date": "2016/04/27",
+      "numOunces": 40
+    },
+    {
+      "userID": 4,
+      "date": "2019/03/15",
+      "numOunces": 35
+    },
+    {
+      "userID": 4,
+      "date": "2019/09/20",
+      "numOunces": 40
+    },
+    {
+      "userID": 4,
+      "date": "2019/09/19",
+      "numOunces": 30
+    },
+    {
+      "userID": 4,
+      "date": "2019/09/18",
+      "numOunces": 40
+    },
+    {
+      "userID": 4,
+      "date": "2019/09/17",
+      "numOunces": 40
+    },
+    {
+      "userID": 4,
+      "date": "2019/09/16",
+      "numOunces": 30
+    },
+    {
+      "userID": 4,
+      "date": "2019/09/15",
+      "numOunces": 30
+    },
     ]
 
     hydration = new Hydration(hydrationData);
   });
 
-  it('should take in a list of data', function() {
+  it('should be a function', () => {
+    expect(Hydration).to.be.a('function');
+    expect(hydration).to.be.an.instanceof(Hydration);
+  });
+
+  it('should take in a list of data', function () {
     expect(hydration.hydrationData[0].userID).to.equal(1);
     expect(hydration.hydrationData[2].numOunces).to.equal(1);
     expect(hydration.hydrationData[4].date).to.equal('2018/10/23');
   });
 
-  it('should find the average water intake per day for a user', function() {
+  it('should find the average water intake per day for a user', function () {
     expect(hydration.calculateAverageOunces(3)).to.equal(2);
   });
 
-  it('should find the water intake for a user on a specified date', function() {
+  it('should find the water intake for a user on a specified date', function () {
     expect(hydration.calculateDailyOunces(1, "2019/06/15")).to.equal(37);
     expect(hydration.calculateDailyOunces(4, "2019/04/15")).to.equal(36);
   });
 
-  it('should find water intake by day for first week', function() {
+  it('should find water intake by day for first week', function () {
     const user3 = new User({
       id: 3,
       name: "The Rock",
@@ -145,7 +150,7 @@ describe('Hydration', function() {
     expect(hydration.calculateFirstWeekOunces(userRepo, 4)[6]).to.eql('2019/04/15: 36');
   });
 
-  it('should find sleep quality by day for that days week', function() {
+  it('should find sleep quality by day for that days week', function () {
     const user3 = new User({
       id: 3,
       name: "The Rock",
