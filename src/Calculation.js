35149bd0e1f31a07cbef84f5a18d37c5e0756903
Alex Thompson <ahthomps1@gmail.com>
2021-02-23 18:41:40 -0700

    Create a method to calculate an average

--- src/Calculation.js
+++ src/Calculation.js
@@ -2,6 +2,13 @@ class Calculation {
   constructor(dataSet) {
     this.dataSet;
   }
+  calculateAverage(id, property) {
+    const perDay = this.dataSet.filter(data => id === data.userID);
+    const sum = perDay.reduce((sumSoFar, data) => {
+      return sumSoFar += data[property];
+    }, 0);
+    return Math.round(sum / perDay.length)
+  }
 
 }
 
