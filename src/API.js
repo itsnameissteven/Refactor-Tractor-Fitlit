894624ed867830d085eacaec3612e3332b1c87a6
Anneke Miers <annekemiers.am@gmail.com>
2021-02-24 13:10:26 -0700

    Add post functions and refactored possible addNewData function

--- src/API.js
+++ src/API.js
@@ -1,3 +1,6 @@
+
+// VIEW DATA
+
 const fetchedUserData = () => {
   fetch("http://localhost:3001/api/v1/users")
     .then(response => response.json())
@@ -31,4 +34,51 @@ const fetchAllData = () => {
   fetchedSleepData()
   fetchedActivityData()
   fetchedHydrationData()
+}
+
+// ADD NEW DATA
+
+const addNewSleepData = (newData) => {
+  fetch("http://localhost:3001/api/v1/sleep", {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newData)
+  })
+    .then(response => response.json())
+    .then(sleepData => console.log(sleepData))
+    .catch(err => console.log(err))
+}
+
+const addNewActivityData = (newData) => {
+  fetch("http://localhost:3001/api/v1/activity", {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newData)
+  })
+    .then(response => response.json())
+    .then(activityData => console.log(activityData))
+    .catch(err => console.log(err))
+}
+
+const addNewHydrationData = (newData) => {
+  fetch("http://localhost:3001/api/v1/hydration", {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newData)
+  })
+    .then(response => response.json())
+    .then(hydrationData => console.log(hydrationData))
+    .catch(err => console.log(err))
+}
+
+// ADD NEW DATA REFACTORED POSSIBILITY
+const addNewData = (link, newData) => {
+  fetch(link, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newData)
+  })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
 }
